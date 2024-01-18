3dca5b26e9b3442b64d88a906366d5682107c6e6
Caden Carlson <caden@Cadens-MacBook-Pro.local>
2024-01-18 14:34:16 -0800

    Redirect user to secret page if registered

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, redirect, render_template, jsonify
+from flask import Flask, request, redirect, render_template, jsonify, session
 from models import db, connect_db, User
+from forms import registerForm
 
 app = Flask(__name__)
 
@@ -14,5 +15,31 @@ connect_db(app)
 @app.route("/")
 def root():
     """Redirect to register page"""
+    
 
     return redirect("/register")
+
+@app.route("/register")
+def register():
+    """Show register form so that new users can sign up"""
+
+    form = registerForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+
+        user = User(username, password, email, first_name, last_name)
+
+        db.session.commit()
+        session["username"] = user.username
+
+        return redirect("/secret")
+
+    else:
+        return render_template("register.html", form=form)
+
+
